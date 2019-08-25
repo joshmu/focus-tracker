1b0b3ef0334b69b75df3a70fd3932bb0bacf2208
Josh Mu <mu@joshmu.com>
2019-08-25 18:25:28 +1000

    working jxa grab current window

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-const runApplescript = require('run-applescript');
+const runJxa = require('run-jxa');
+
+const sleep = async ms => new Promise(res => {
+    console.log(`sleep: ${ms}ms`)
+    setTimeout(res, ms)
+})
+
+const getFocus = async () => {
+    const result = await runJxa((unicorn, horse) => {
+        // This is run in the JXA engine
+        let appName = Application('System Events').applicationProcesses.where({ frontmost: true }).name()[0]
+        // const frontmost_app = Application(frontmost_app_name)
+        if (appName === 'Brave Browser') {
+            // Brave
+            Application(appName).windows[0].activeTab.name()
+            let url = Application(appName).windows[0].activeTab.url()
+            appName = url.split('//')[1].split('/')[0]
+        }
+        return appName
+    }, ['🦄', '🐴']);
+
+    return result
+}
 
 (async () => {
-	const result = await runApplescript('return "unicorn"');
-
-	console.log(result);
-	//=> 'unicorn'
-})();
+    await sleep(10000)
+    let focus = await getFocus()
+    console.log({focus})
+})()
