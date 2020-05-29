a2b7286600998369379660ea85fd88fda6ffeff3
Josh Mu <mu@joshmu.com>
2020-05-29 13:14:24 +1000

    added pm2 ecosystem.config

---
+++ ft-pm2.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    apps : [{
+        name        : "focus-tracker",
+        script      : "./index.js",
+        watch       : true,
+        instances   : 1,
+        ignore_watch: [”*.json”, “node_modules”],
+        max_restarts: 10
+    }]
+}
